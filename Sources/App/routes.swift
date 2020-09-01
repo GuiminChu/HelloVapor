efaa22588a62840a71d923d7c768dec7cdb63d85
Guimin Chu <455824173@qq.com>
2020-09-01 14:16:58 +0800

    learn routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,24 @@ func routes(_ app: Application) throws {
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+    
+    app.get("hello", ":name") { req -> String in
+        guard let name = req.parameters.get("name") else {
+            throw Abort(.internalServerError)
+        }
+        return "Hello \(name)"
+    }
+    
+    app.post("info") { req -> InfoResponse in
+        let data = try req.content.decode(InfoData.self)
+        return InfoResponse(req: data)
+    }
+}
+
+struct InfoData: Content {
+    let name: String
+}
+
+struct InfoResponse: Content {
+    let req: InfoData
 }
